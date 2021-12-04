889dd072f4894b6ba815e4ff353c35ea85374826
Matthew Ty <matthewty1220@gmail.com>
2021-12-04 13:27:43 -0700

    added counting sheep problem(solved)

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function remainder(a, b) {
 
 
 // Counting sheep...
-// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of present in the arrray (true means present).
+// Consider an array/list of sheep where some sheep may be missing from their place. 
+// We need a function that counts the number of present in the arrray (true means present).
 function countSheeps(arrayOfSheep) {
     let num = 0;
     for(let i = 0; i < arrayOfSheep.length; i++)
