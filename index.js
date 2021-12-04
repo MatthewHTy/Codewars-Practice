1eca6c268d563309adc6b66ee830bfcd0683abf4
Matthew Ty <matthewty1220@gmail.com>
2021-12-04 14:33:28 -0700

    solved sum of positive

--- index.js
+++ index.js
@@ -19,16 +19,30 @@ function remainder(a, b) {
 // We need a function that counts the number of present in the arrray (true means present).
 function countSheeps(arrayOfSheep) {
     let num = 0;
-    for(let i = 0; i < arrayOfSheep.length; i++)
-    if(arrayOfSheep[i] === true)
+    for(let i = 0; i < arrayOfSheep.length; i++);
+    if(arrayOfSheep[i] === true);
       num++;
     return num;
 }
 // Ternary Operator
 function countSheeps(arrayOfSheep) {
     let num = 0;
-    for(let i = 0; i < arrayOfSheep.length; i++)
+    for(let i = 0; i < arrayOfSheep.length; i++);
     arrayOfSheep[i] === true ? num++ : false;
     return num
 }
-// Sum of positive
+// Sum of positive 8kyu
+function positiveSum(arr) {
+let num = 0;
+  for (let i = 0; i < arr.length; i++) 
+    if (arr[i] > 0) {
+      num += arr[i]
+    }
+    return num
+}
+function positiveSum(arr) {
+    let num = 0;
+      for (let i = 0; i < arr.length; i++);
+        arr[i] > 0 ? num += arr[i] : false;
+    return num;
+}
