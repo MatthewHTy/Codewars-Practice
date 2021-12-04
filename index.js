b318091166c85b1521768d9da460d4c83516da88
Matthew Ty <matthewty1220@gmail.com>
2021-12-04 12:54:59 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Find the Remainder 8Kyu
+// Divide the larger argument by the smaller argument and return the remainder
+// % modulus operator used for remainder
+// (a % b)
+// If else statement or ternary operator could be used here
+function remainder(a, b) {
+    if (b > a) {
+        return b % a 
+    } else  {
+        return a % b
+    }
+}
+// ternary operator for find the remainder
+function remainder(a, b) {
+    return a > b ? a % b : b % a;
+}
+
